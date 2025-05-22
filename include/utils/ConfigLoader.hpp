a0b9ddd2eac8d918f1154cebecf85e0e618f2baf
manuelmusngi <25419759+manuelmusngi@users.noreply.github.com>
2025-05-22 15:33:59 -0400

    Create ConfigLoader.hpp

---
+++ include/utils/ConfigLoader.hpp
@@ -0,0 +1,14 @@
+#ifndef CONFIG_LOADER_HPP
+#define CONFIG_LOADER_HPP
+
+#include <string>
+#include <unordered_map>
+
+class ConfigLoader {
+public:
+    bool loadConfig(const std::string& filepath);
+    std::string getValue(const std::string& key) const;
+
+private:
+    std::unordered_map<std::string, std::string> configMap;
+};
