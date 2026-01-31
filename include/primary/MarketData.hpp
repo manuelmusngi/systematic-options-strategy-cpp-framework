0a94a85857ae04772948d9d5859848cb974ba4ba
manuelmusngi <25419759+manuelmusngi@users.noreply.github.com>
2026-01-31 07:03:45 -0500

    Create MarketData.hpp

---
+++ include/primary/MarketData.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+#include <functional>
+#include <chrono>
+#include <vector>
+
+struct Quote {
+    std::string symbol;
+    double bid{};
+    double ask{};
+    double last{};
+    std::chrono::system_clock::time_point timestamp{};
+};
+
+class MarketData {
+public:
+    using QuoteCallback = std::function<void(const Quote&)>;
+
+    virtual ~MarketData() = default;
+
+    virtual void subscribe(const std::string& symbol) = 0;
+    virtual void unsubscribe(const std::string& symbol) = 0;
+    virtual void setQuoteCallback(QuoteCallback cb) = 0;
+    virtual void start() = 0;
+    virtual void stop() = 0;
+};
