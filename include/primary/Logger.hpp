37afd8066ac034e8dc54c4161decf679a1266d38
manuelmusngi <25419759+manuelmusngi@users.noreply.github.com>
2026-02-02 19:04:30 -0500

    Create Logger.hpp

---
+++ include/primary/Logger.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include <string_view>
+
+enum class LogLevel { Debug, Info, Warn, Error };
+
+class Logger {
+public:
+    virtual ~Logger() = default;
+
+    virtual void log(LogLevel level, std::string_view msg) = 0;
+
+    void info(std::string_view msg) { log(LogLevel::Info, msg); }
+    void debug(std::string_view msg) { log(LogLevel::Debug, msg); }
+    void warn(std::string_view msg) { log(LogLevel::Warn, msg); }
+    void error(std::string_view msg) { log(LogLevel::Error, msg); }
+};
